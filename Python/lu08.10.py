4fff3f558e8a608922d4e7a325cba2a0f5f3f944
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2025-10-13 10:33:57 +0300

    lab exercise from 08.10

---
+++ Python/lu08.10.py
@@ -0,0 +1,56 @@
+import math
+
+# print("Hello world!!!")
+# print("💖")
+
+# a = 8
+# b = 4
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+
+
+# firstStr = input("Enter first word: ")
+# secondStr = input("Enter second word: ")
+# print(f"{firstStr} + {secondStr}")
+
+# print([1,2] + [3,4])
+
+# str = input("Enter string: ")
+# n = int(input("Enter number: "))
+# print(str * n)
+
+# print('e' in "hello")
+# print(2 in [1,2,3])
+
+# student = {
+#     "име" : "Иван",
+#     "град" : "София" 
+# }
+
+# a = int(input("Enter number 1: "))
+# b = int(input("Enter number 2: "))
+# s = input("Enter name of variable: ")
+# print(f"a={a}; b={a}; area={a*b}")
+
+# a = int(input("Enter number a: "))
+# b = int(input("Enter number b: "))
+# h = int(input("Enter number h: "))
+# print(f"Area is {((a+b)*h)/2}")
+
+# R = int(input("Enter number R: "))
+# area = math.PI * R * R
+# area = round(area, 3)
+# perimiter = 2 * math.pi * R
+# perimiter = round(perimiter, 3)
+# print(f"Area is {area}")
+# print(f"Perimiter is {perimiter}")
+
+hourlyRate = float(input("Enter rate: "))
+hours = int(input("Enter hours: "))
+print(hourlyRate*hours)
+
+
