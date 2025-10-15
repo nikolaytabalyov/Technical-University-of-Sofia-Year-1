782810f228077d50bbfff8cf2cbfc6276d0a21d2
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2025-10-15 14:43:37 +0300

    added python files from lecture and lab exrcs

---
+++ Python/lu15.10.py
@@ -0,0 +1,37 @@
+# # zadacha 1
+# print("Задача 1")
+# n = int(input("Enter number n: "))
+# max = 0
+# for i in range(n):
+#     num = int(input(f"Enter number {i + 1}: "))
+#     if num <= 0:
+#         raise Exception("The number is not > 0")
+#     if num > max:
+#         max = num   
+
+# print(f"The biggest number is {max}")
+
+# # zadacha 2
+# print("Задача 2")
+# n = int(input("Enter number n: "))
+# sum = 0
+# for i in range(n):
+#     sum += int(input(f"Enter number {i + 1}: "))
+
+# print(f"The sum is {sum}")
+
+# # zadacha 3
+# print("Задача 3")
+# n = int(input("Enter number n: "))
+# for i in range (0, n + 1):
+#     print('*' * i)
+    
+# # zadacha 4
+# print("Задача 4")
+# n = int(input("Enter number n: "))
+# is_prime = True
+# for i in range (2, n):
+#     if n % i == 0:
+#         is_prime = False
+
+# print(f"The number is prime: {is_prime}")
---
+++ Python/matrices.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+m = np.matrix([1,2], [4,5])
+print(m)
